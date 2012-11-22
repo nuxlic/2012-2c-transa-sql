66914011301a98bb84cf05ce88d427b699950a20
nuxlic <cangini.f@hotmail.com>
2012-11-22 00:16:50 -0300

    application de altaProveedor

---
+++ src/GrouponDesktop/AbmProveedor/AltaProvApp.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Windows.Forms;
+using GrouponDesktop.Commons.Database;
+
+namespace GrouponDesktop.AbmProveedor
+{
+    public class AltaProvApp
+    {
+
+        private string _cityId;
+
+        public string CityId
+        {
+            get { return _cityId; }
+            set { _cityId = value; }
+        }
+        private string _entryId;
+
+        public string EntryId
+        {
+            get { return _entryId; }
+            set { _entryId = value; }
+        }
+        
+        private string _userId;
+        private string _personalDataId;
+
+        public string PersonalDataId
+        {
+            get { return _personalDataId; }
+            set { _personalDataId = value; }
+        }
+        public string UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        private string _razonSocial;
+
+        public string RazonSocial
+        {
+            get { return _razonSocial; }
+            set { _razonSocial = value; }
+        }
+        private string _mail;
+
+        public string Mail
+        {
+            get { return _mail; }
+            set { _mail = value; }
+        }
+        private string _telefono;
+
+        public string Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+        private string _direccion;
+
+        public string Direccion
+        {
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+        private string _codigoPostal;
+
+        public string CodigoPostal
+        {
+            get { return _codigoPostal; }
+            set { _codigoPostal = value; }
+        }
+        private string _ciudad;
+
+        public string Ciudad
+        {
+            get { return _ciudad; }
+            set { _ciudad = value; }
+        }
+        private string _cuit;
+
+        public string Cuit
+        {
+            get { return _cuit; }
+            set { _cuit = value; }
+        }
+        private string _rubro;
+
+        public string Rubro
+        {
+            get { return _rubro; }
+            set { _rubro = value; }
+        }
+        private string _numeroContac;
+
+        public string NumeroContac
+        {
+            get { return _numeroContac; }
+            set { _numeroContac = value; }
+        }
+
+        public bool existeProveedor()
+        {
+            StringBuilder sentence = new StringBuilder().Append("select * from TRANSA_SQL.Supplier s where s.CorporateName='").Append(this.RazonSocial).Append("' and s.Cuit='").Append(this.Cuit).Append("'");
+            DataTable table = Conexion.Instance.ejecutarQuery(sentence.ToString());
+            return table.Rows.Count > 0;
+        }
+
+
+        public string dameMiRoleId()
+        {
+            StringBuilder sentence = new StringBuilder().Append("select * from TRANSA_SQL.Role r where r.Name='Supplier'");
+            DataTable table = Conexion.Instance.ejecutarQuery(sentence.ToString());
+            return table.Rows[0]["RoleId"].ToString();
+        }
+
+        public void crearUsuario()
+        {
+            StringBuilder sentence = new StringBuilder().Append("insert into TRANSA_SQL.CuponeteUser values ('").Append(this.Cuit).Append("',").Append("'47f5390d283f8cbcc8272dbc288b2cae42ec57d13cb8abea14cd7754f2be57dd',").Append("0,").Append(this.dameMiRoleId()).Append(",1,0)");
+            DataTable table = Conexion.Instance.ejecutarQuery(sentence.ToString());
+            this.UserId= table.Rows[0]["UserId"].ToString();
+        }
+
+        public void crearPersonalData()
+        {
+            StringBuilder sentence = new StringBuilder().Append("insert into TRANSA_SQL.PersonalData values (").Append(this.UserId).Append(", '").Append(this.Mail).Append("','").Append(this.CodigoPostal).Append("','").Append(this.Direccion).Append("')");
+            DataTable table = Conexion.Instance.ejecutarQuery(sentence.ToString());
+            this.PersonalDataId = table.Rows[0]["PersonalDataId"].ToString();
+        }
+
+        public string getOrSetCityId()
+        {
+            string toReturn=null;
+            StringBuilder sentence = new StringBuilder().Append("select * from TRANSA_SQL.City c where c.Name='").Append(this.Ciudad).Append("'");
+            DataTable table = Conexion.Instance.ejecutarQuery(sentence.ToString());
+            if (table.Rows.Count > 0)
+            {
+                return table.Rows[0]["CityId"].ToString();
+            }
+            else
+            {
+                StringBuilder sentce = new StringBuilder().Append("insert into TRANSA_SQL.City values ('").Append(this.Ciudad).Append("')");
+                DataTable tab = Conexion.Instance.ejecutarQuery(sentce.ToString());
+                toReturn = tab.Rows[0]["CityId"].ToString();
+            }
+            return toReturn;
+        }
+
+        public string getOrSetEntryId()
+        {
+            string toReturn = null;
+            StringBuilder sentence = new StringBuilder().Append("select * from TRANSA_SQL.Entry e where e.Name='").Append(this.Rubro).Append("'");
+            DataTable table = Conexion.Instance.ejecutarQuery(sentence.ToString());
+            if (table.Rows.Count > 0)
+            {
+                return table.Rows[0]["EntryId"].ToString();
+            }
+            else
+            {
+                StringBuilder sentce = new StringBuilder().Append("insert into TRANSA_SQL.Entry values ('").Append(this.Rubro).Append("')");
+                DataTable tab = Conexion.Instance.ejecutarQuery(sentce.ToString());
+                toReturn = tab.Rows[0]["EntryId"].ToString();
+            }
+            return toReturn;
+        }
+
+        public void crearSupplier()
+        {
+            this.EntryId = getOrSetEntryId();
+            this.CityId = getOrSetCityId();
+            if (!existeProveedor())
+            {
+                StringBuilder sentence = new StringBuilder().Append("insert into TRANSA_SQL.Supplier values ('").Append(this.RazonSocial).Append("', '").Append(this.Cuit).Append("',").Append(this.UserId).Append(",").Append(this.Telefono).Append(",1, ").Append(this.CityId).Append(", ").Append(this.EntryId).Append(", '").Append(this.NumeroContac).Append("', ").Append(this.PersonalDataId).Append(")");
+                Conexion.Instance.ejecutarQuery(sentence.ToString());
+                MessageBox.Show("El Proveedor ha sido de alta con exito", "Informacion:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El Proveedor ya existe en el sistema", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+    }
+}
---
+++ src/GrouponDesktop/AbmProveedor/AltaProvForm.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace GrouponDesktop.AbmProveedor
+{
+    partial class AltaProvForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AltaProvForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.corporateName = new System.Windows.Forms.TextBox();
+            this.Mail = new System.Windows.Forms.TextBox();
+            this.Phone = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.TextBox();
+            this.postalCode = new System.Windows.Forms.TextBox();
+            this.City = new System.Windows.Forms.TextBox();
+            this.Cuit = new System.Windows.Forms.TextBox();
+            this.ContactNumber = new System.Windows.Forms.TextBox();
+            this.Limpiar = new System.Windows.Forms.Button();
+            this.guardar = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.entry = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese los datos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Razon Social";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Direccion (calle,nro,piso,depto)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Codigo Postal";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Ciudad";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(14, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Cuit";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 318);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(105, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Nombre de Contacto";
+            // 
+            // corporateName
+            // 
+            this.corporateName.Location = new System.Drawing.Point(192, 72);
+            this.corporateName.Name = "corporateName";
+            this.corporateName.Size = new System.Drawing.Size(100, 20);
+            this.corporateName.TabIndex = 9;
+            // 
+            // Mail
+            // 
+            this.Mail.Location = new System.Drawing.Point(192, 99);
+            this.Mail.Name = "Mail";
+            this.Mail.Size = new System.Drawing.Size(100, 20);
+            this.Mail.TabIndex = 10;
+            // 
+            // Phone
+            // 
+            this.Phone.Location = new System.Drawing.Point(192, 126);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(100, 20);
+            this.Phone.TabIndex = 11;
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(192, 164);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(100, 20);
+            this.address.TabIndex = 12;
+            // 
+            // postalCode
+            // 
+            this.postalCode.Location = new System.Drawing.Point(192, 193);
+            this.postalCode.Name = "postalCode";
+            this.postalCode.Size = new System.Drawing.Size(100, 20);
+            this.postalCode.TabIndex = 13;
+            // 
+            // City
+            // 
+            this.City.Location = new System.Drawing.Point(192, 220);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(100, 20);
+            this.City.TabIndex = 14;
+            // 
+            // Cuit
+            // 
+            this.Cuit.Location = new System.Drawing.Point(192, 251);
+            this.Cuit.Name = "Cuit";
+            this.Cuit.Size = new System.Drawing.Size(100, 20);
+            this.Cuit.TabIndex = 15;
+            // 
+            // ContactNumber
+            // 
+            this.ContactNumber.Location = new System.Drawing.Point(192, 318);
+            this.ContactNumber.Name = "ContactNumber";
+            this.ContactNumber.Size = new System.Drawing.Size(100, 20);
+            this.ContactNumber.TabIndex = 16;
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Location = new System.Drawing.Point(13, 368);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(75, 23);
+            this.Limpiar.TabIndex = 17;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            // 
+            // guardar
+            // 
+            this.guardar.Location = new System.Drawing.Point(217, 368);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(75, 23);
+            this.guardar.TabIndex = 18;
+            this.guardar.Text = "Guardar";
+            this.guardar.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 282);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Rubro";
+            // 
+            // entry
+            // 
+            this.entry.Location = new System.Drawing.Point(192, 282);
+            this.entry.Name = "entry";
+            this.entry.Size = new System.Drawing.Size(100, 20);
+            this.entry.TabIndex = 20;
+            // 
+            // AltaProvForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 403);
+            this.Controls.Add(this.entry);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.ContactNumber);
+            this.Controls.Add(this.Cuit);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.postalCode);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.Mail);
+            this.Controls.Add(this.corporateName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AltaProvForm";
+            this.Text = "Alta Proveedor";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox corporateName;
+        private System.Windows.Forms.TextBox Mail;
+        private System.Windows.Forms.TextBox Phone;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.TextBox postalCode;
+        private System.Windows.Forms.TextBox City;
+        private System.Windows.Forms.TextBox Cuit;
+        private System.Windows.Forms.TextBox ContactNumber;
+        private System.Windows.Forms.Button Limpiar;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox entry;
+    }
+}
