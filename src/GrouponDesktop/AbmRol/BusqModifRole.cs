50ed00c721e76885f8b90372e09f84742003fb3b
flopezluksenbreg <flopezluksenberg@gmail.com>
2012-12-04 17:50:55 -0300

    ABM Role Terminado. Ahora subo lo que va en el script

---
+++ src/GrouponDesktop/AbmRol/BusqModifRole.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GrouponDesktop.AbmCliente;
+using GrouponDesktop.Commons.Database;
+
+namespace GrouponDesktop.AbmRol
+{
+    public partial class BusqModifRole : Form
+    {
+        private BusquedaApp model = new BusquedaApp();
+        public BusqModifRole()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (this.txtName.Text != "") this.model.Name = this.txtName.Text;
+            this.dataGridView2.DataSource = this.model.buscar();
+            this.dataGridView2.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+        }
+
+        
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtName.Text = "";
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            new AltaRolApp().validarSoloLetrasYnumeros(e);
+        }
+
+        protected virtual void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0 && e.RowIndex >= 0)
+            {
+                StringBuilder sentence = new StringBuilder().AppendFormat("SELECT R.RoleId FROM TRANSA_SQL.Role R WHERE R.Name='{0}'", this.dataGridView2.CurrentRow.Cells[1].Value);
+                int roleId = (int)Conexion.Instance.ejecutarQuery(sentence.ToString()).Rows[0][0];
+                ModificacionRoleForm modif = new ModificacionRoleForm(roleId);
+                this.Hide();
+                modif.ShowDialog();
+                this.Show();
+                this.btnBuscar.PerformClick();
+            }
+        }
+    }
+}
---
+++ src/GrouponDesktop/AbmRol/BusquedaApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using GrouponDesktop.Commons.Database;
+
+namespace GrouponDesktop.AbmRol
+{
+    class BusquedaApp
+    {
+        public string Name { get; set; }
+        public DataTable buscar()
+        {
+            DataTable tabla = new DataTable();
+
+            Conexion cnn = Conexion.Instance;
+            System.Data.SqlClient.SqlCommand comando1 = new System.Data.SqlClient.SqlCommand();
+            comando1.CommandType = CommandType.StoredProcedure;
+
+            if (this.Name != "")
+            {
+                comando1.Parameters.Add("@Name", SqlDbType.NVarChar);
+                comando1.Parameters[0].Value = this.Name;
+            }
+            comando1.CommandText = "TRANSA_SQL.filtrarRole";
+            tabla = cnn.ejecutarQueryConSP(comando1);
+
+            return tabla;
+        }
+    }
+}
