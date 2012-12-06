83857e2c1fe524782985aa4940701423b001cf1c
flopezluksenbreg <flopezluksenberg@gmail.com>
2012-12-06 19:53:45 -0300

    TP TERMINADO POSTA 2 FINAL :D :D :D :D CODIGO By: 98%: Flavio Cangini - 2% Federico Lopez LUksenberg :D!!!

--- src/GrouponDesktop/CargaCredito/TargetaForm.Designer.cs
+++ src/GrouponDesktop/CargaCredito/TargetaForm.Designer.cs
@@ -35,15 +35,17 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 51);
+            this.panel1.Size = new System.Drawing.Size(287, 51);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -67,16 +69,16 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 81);
+            this.textBox1.Location = new System.Drawing.Point(83, 81);
             this.textBox1.MaxLength = 8;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(63, 138);
+            this.button1.Location = new System.Drawing.Point(144, 133);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 34);
             this.button1.TabIndex = 3;
@@ -86,7 +88,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(144, 138);
+            this.button2.Location = new System.Drawing.Point(225, 133);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 34);
             this.button2.TabIndex = 4;
@@ -94,11 +96,20 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(283, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ATENCION! SOLO TRABAJAMOS CON TARJETAS VISA";
+            // 
             // TargetaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 179);
+            this.ClientSize = new System.Drawing.Size(312, 179);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -123,5 +134,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
     }
 }
